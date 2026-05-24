5f68cf94ef3b1267f2636874b057f352cad425a0
Sailaja5125 <sailajapuvala5125@gmail.com>
2026-05-24 14:41:51 +0530

    all done

--- blogApi/database/db.js
+++ blogApi/database/db.js
@@ -4,8 +4,8 @@ import dns from "dns";
 dns.setServers(["1.1.1.1","8.8.8.8"]);
 const DataBase = async()=>{
     try {
-        const url = "mongodb://sailajapuvala5125_db_user:Rf5FgJeXwn95Uunw@cluster0.bzkha91.mongodb.net/?appName=Cluster0"
-        await mongoose.connect(process.env.MONGO_DB_SECONDARY || url);
+        // const url = "mongodb://sailajapuvala5125_db_user:Rf5FgJeXwn95Uunw@cluster0.bzkha91.mongodb.net/?appName=Cluster0"
+        await mongoose.connect(process.env.MONGO_DB_SECONDARY );
         console.log("Database connected successfully");
     } catch (error) {
         console.log("Database connection failed",error);
